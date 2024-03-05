2e9760206e8e48eea62603a216b201f96aa129f9
Maquinadelmal23110112 <159966461+Maquinadelmal23110112@users.noreply.github.com>
2024-03-05 22:58:11 +0000

    Version 5

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 #include<Mascota.hpp>
 #include<Alimento.hpp>
 
@@ -9,14 +10,21 @@ int main(int argc, char const *argv[])
     Mascota m1;
     Mascota m2;
 
+    mascotas.pushback;
+
     Alimento a1(5);
     Alimento a2(2);
 
    m1.Comer(a1);
    m2.Comer(a2);
 
-    cout << "Mascota 1, Energia:"<<m1.LeerEnergia() <<endl
-         << "Mascota 2, Energia:"<<m2.LeerEnergia() <<endl;
-   
+   for (auto &&mascota : _mascotas)
+   {
+       std::cout << "Mascota 1, Energia:"
+                 << m1.LeerEnergia()
+                        std::
+                 << endl;
+   }
+
     return 0;
 }
